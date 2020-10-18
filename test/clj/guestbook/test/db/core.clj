23ab3aea457366f5cb2175a0d769eb81d889a505
bishax <alex.bishop@nesta.org.uk>
2020-10-18 15:06:31 +0100

    Add tests for guestbook queries

--- test/clj/guestbook/test/db/core.clj
+++ test/clj/guestbook/test/db/core.clj
@@ -17,22 +17,18 @@
     (migrations/migrate ["migrate"] (select-keys env [:database-url]))
     (f)))
 
-(deftest test-users
+(deftest test-guestbook
   (jdbc/with-db-transaction [t-conn *db*]
     (jdbc/db-set-rollback-only! t-conn)
-    (is (= 1 (db/create-user!
+    ; Test save
+    (is (= 1 (db/save-message!
               t-conn
-              {:id         "1"
-               :first_name "Sam"
-               :last_name  "Smith"
-               :email      "sam.smith@example.com"
-               :pass       "pass"})))
-    (is (= {:id         "1"
-            :first_name "Sam"
-            :last_name  "Smith"
-            :email      "sam.smith@example.com"
-            :pass       "pass"
-            :admin      nil
-            :last_login nil
-            :is_active  nil}
-           (db/get-user t-conn {:id "1"})))))
+              {:name "Bob"
+               :message "Hello, world!"}
+              {:connection t-conn})))
+    ; Test get-messages
+    (is (= {:name "Bob"
+            :message "Hello, world!"}
+           (-> (db/get-messages t-conn {})
+               (first)
+               (select-keys [:name :message]))))))
