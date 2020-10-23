fc03f219a58c5bfd07261ad1bb8ba0910a42d286
bishax <alex.bishop@nesta.org.uk>
2020-10-23 10:18:57 +0100

    Use clojurescript with re-frame for front-end (#1)
    * Shares message validation between client and server
    * state management using re-frame events & subs
     - :messages/list sub for reading
     - :messages/set to set messages retrieved from server
     - :messages/add to add a new message

--- src/clj/guestbook/routes/home.clj
+++ src/clj/guestbook/routes/home.clj
@@ -6,36 +6,26 @@
    [guestbook.middleware :as middleware]
    [ring.util.response]
    [ring.util.http-response :as response]
-   [struct.core :as st]))
+   [guestbook.validation :refer [validate-message]]))
 
-(defn home-page [{:keys [flash] :as request}]
-  (layout/render request "home.html" (merge {:messages (db/get-messages)}
-                                            (select-keys flash [:name :message :errors]))))
+(defn home-page [request]
+  (layout/render request "home.html"))
 
 (defn about-page [request]
   (layout/render request "about.html"))
 
-(def message-schema
-  [[:name
-    st/required
-    st/string]
-   [:message
-    st/required
-    st/string
-    {:message "message must contain at least 10 characters"
-     :validate (fn [msg] (>= (count msg) 10))}]])
-
-(defn validate-message [params]
-  (first (st/validate params message-schema)))
-
 (defn save-message! [{:keys [params]}]
   (if-let [errors (validate-message params)]
-    (-> (response/found "/")
-        (assoc :flash (assoc params :errors errors)))
-    (do
+    (response/bad-request {:errors errors})
+    (try
         (db/save-message! params) ; add message to DB
-        (response/found "/")))) ; Re-direct back on (implicit) success)
+        (response/ok {:status :ok})
+        (catch Exception e
+          (response/internal-server-error
+           {:errors {:server-error ["Failed to save message!"]}})))))
 
+(defn message-list [_]
+  (response/ok {:messages (vec (db/get-messages))}))
 
 (defn home-routes []
   [""
@@ -43,4 +33,5 @@
                  middleware/wrap-formats]}
    ["/" {:get home-page}]
    ["/about" {:get about-page}]
-   ["/message" {:post save-message!}]])
+   ["/message" {:post save-message!}]
+   ["/messages" {:get message-list}]])
---
+++ src/cljs/guestbook/core.cljs
@@ -0,0 +1,118 @@
+(ns guestbook.core
+  (:require [reagent.core :as r]
+            [reagent.dom :as dom]
+            [re-frame.core :as rf]
+            [ajax.core :refer [GET POST]]
+            [clojure.string :as string]
+            [guestbook.validation :refer [validate-message]]))
+
+
+(defn message-form []
+  (let [fields (r/atom {})
+        errors (r/atom nil)]
+    (fn []
+      [:div
+       [errors-component errors :server-error]
+       [:div.field
+        [:label.label {:for :name} "Name"]
+        [errors-component errors :name]
+        [:input.input {:type :text
+                       :name :name
+                       :value (:name @fields)
+                       :on-change #(swap! fields assoc :name (-> % .-target .-value))}]]
+       [:div.field
+        [:label.label {:for :message} "Message"]
+        [errors-component errors :message]
+        [:textarea.textarea {:name :message
+                             :value (:message @fields)
+                             :on-change #(swap! fields assoc :message (-> % .-target .-value))}]]
+       [:input.button.is-primary
+        {:type :submit
+         :value "comment"
+         :on-click #(send-message! fields errors)}]])))
+
+(rf/reg-event-db
+ ; "DB event for adding new message"
+ :message/add
+ (fn [db [_ message]]
+   (update db :messages/list conj message)))
+
+(defn send-message! [fields errors]
+  (if-let [validation-errors (validate-message @fields)]
+    (reset! errors validation-errors)
+    (POST "/message"
+        {:format :json
+         :headers {"Accept" "application/transit+json"
+                   "x-csrf-token" (.-value (.getElementById js/document "token"))}
+         :params @fields
+         :handler #(do
+                     (rf/dispatch [:message/add (assoc @fields :timestamp (js/Date.))])
+                     (reset! fields nil)
+                     (reset! errors nil))
+         :error-handler #(do
+                           (.log js/console (str %))
+                           (reset! errors (get-in % [:response :errors])))})))
+
+(defn errors-component [errors id]
+  (when-let [error (id @errors)]
+        [:div.notification.is-danger (string/join error)]))
+
+(rf/reg-event-db
+ ;"DB event for getting messages"
+ :messages/set
+ (fn [db [_ messages]]
+   (-> db
+       (assoc :messages/loading? false
+              :messages/list messages))))
+
+(rf/reg-sub
+ ;"Subscription for getting messages"
+ :messages/list
+ (fn [db _]
+   (:messages/list db [])))
+
+(defn get-messages []
+  (GET "/messages"
+       {:headers {"Accept" "application/transit+json"}
+        :handler #(rf/dispatch [:messages/set (:messages %)])}))
+
+(defn message-list [messages]
+  [:ul.messages
+   (for [{:keys [timestamp message name]} @messages]
+    ^{:key timestamp}
+     [:li
+      [:time (.toLocaleString timestamp)]
+      [:p message]
+      [:p " - " name]])])
+
+(rf/reg-event-fx
+ ;"Initialisation event, while loading data"
+ :app/initialize
+ (fn [_ _]
+   {:db {:messages/loading? true}}))
+
+
+(rf/reg-sub
+ ;"Subscription for message loading"
+ :messages/loading?
+ (fn [db _]
+   (:messages/loading? db)))
+
+(defn home []
+  (let [messages (rf/subscribe [:messages/list])]
+    (rf/dispatch [:app/initialize])
+    (get-messages)
+    (fn []
+      [:div.content>div.columns.is-centered>div.column.is-two-thirds
+       (if @(rf/subscribe [:messages/loading?])
+        [:h3 "Loading Messages..."]
+        [:div
+         [:div.columns>div.column
+          [:h3 "Messages"]
+          [message-list messages]]
+         [:div.columns>div.column
+          [message-form]]])])))
+
+(dom/render
+ [home]
+ (.getElementById js/document "content"))
