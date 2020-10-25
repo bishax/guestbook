301625a2805ce0f2bf2c486d42976179ed68982a
bishax <alex.bishop@nesta.org.uk>
2020-10-25 12:00:19 +0000

    Re-frame: separate events and effects
    Two generic effects:
    - :ws/send! to send websocket message (and optional callback-event)
    - :ajax/get to send ajax get request
    
    Event :messages/load calls Effect :ajax/get
    Event :messages/send! calls :ws/send! (passing
          :message/send!-called-back as the callback-event)

--- src/cljs/guestbook/core.cljs
+++ src/cljs/guestbook/core.cljs
@@ -16,17 +16,19 @@
    (update db :messages/list conj message)))
 
 (rf/reg-event-fx
+ :message/send!-called-back
+ (fn [_ [_ {:keys [success errors]}]]
+    (if success
+        (rf/dispatch [:form/clear-fields])
+        (rf/dispatch [:form/set-server-errors errors]))))
+
+(rf/reg-event-fx
  :message/send!
  (fn [{:keys [db]} [_ fields]]
-   (ws/send!
-    [:message/create! fields]
-    10000 ; Timeout
-    (fn [{:keys [success errors] :as response}]
-        (.log js/console "Called back: " (pr-str response))
-        (if success
-          (rf/dispatch [:form/clear-fields])
-          (rf/dispatch [:form/set-server-errors errors]))))
-   {:db (dissoc db :form/server-errors)}))
+   {:db (dissoc db :form/server-errors)
+    :ws/send! {:message [:message/create! fields]
+               :timeout 10000
+               :callback-event [:message/send!-called-back]}}))
 
 (defn handle-response! [response]
   (if-let [errors (:errors response)]
@@ -155,13 +157,30 @@
    {:db {:messages/loading? true}
     :dispatch [:messages/load]}))
 
+(rf/reg-fx
+ :ajax/get
+ (fn [{:keys [url success-event error-event success-path]}]
+   (GET url
+        (cond-> {:headers {"Accept" "application/transit+json"}}
+          success-event (assoc :handler
+                               #(rf/dispatch
+                                 (conj success-event
+                                                   (if success-path
+                                                     (get-in % success-path)
+                                                     %))))
+          error-event   (assoc :error-handler
+                               #(rf/dispatch
+                                 (conj error-event %)))))))
+
 (rf/reg-event-fx
  :messages/load
  (fn [{:keys [db]} _]
-  (GET "/api/messages"
-       {:headers {"Accept" "application/transit+json"}
-        :handler #(rf/dispatch [:messages/set (:messages %)])})
-  {:db (assoc db :messages/loading? true)}))
+   {:db       (assoc db :messages/loading? true)
+    :ajax/get {:url           "/api/messages"
+               ; Event to dispatch on success
+               :success-event [:messages/set]
+               ; Path to data in response to pass to event
+               :success-path  [:messages]}}))
 
 (rf/reg-sub
  ;"Subscription for message loading"
